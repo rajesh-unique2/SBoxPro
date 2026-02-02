65423afc65c5fe76962b428f63fd2c34cd681917
rajesh-unique2 <rajeshkrishvanth3@gmail.com>
2026-02-02 20:28:59 +0530

    last commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -6,22 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {fontFamily: {
-        playfair: ['"Playfair Display"', 'serif'],},
+    extend: {
+      fontFamily: {
+        playfair: ['"Playfair Display"', 'serif'],
+      },
+    },
   },
   plugins: [], // add Tailwind plugins here if needed
 }
-=======
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {fontFamily: {
-        playfair: ['"Playfair Display"', 'serif'],},
-  },
-  plugins: [], // add Tailwind plugins here if needed
-}
->>>>>>> 5a3b98344af1ab7b249d358fb2ea493cbee01ece
